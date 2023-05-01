e3890757d47533eff82bc988138c3ec529404d3f
Sandeep Moorani <sandeepmoorani222@gmail.com>
2023-05-01 08:35:27 +0500

    commit added

---
+++ Hello.py
@@ -0,0 +1,22 @@
+# What is Python?
+# Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+# our first python program 
+
+# syntax of python is very simple you just write print and display any thing in double cotted comma's like print("hi")
+
+print("hello world")
+print("its my first program in Python")
+
+#  this symbol is basically used for commint in python programing 
+#  Comments can be used to explain Python code.Comments can be used to make the code more readable.Comments can be used to prevent execution when testing code.
+#  basically we use commits when some lines of code thats not necessary then we use commits.
+
+# for single commit use #
+# for multilines use """ """ this tripple cotted commas
+
+print("iam a \"software Engineer\"")   #this is escape sequence
+
+print("Iam Sandeep Moorani",15, sep="-->")
+print("Good Night")
+
