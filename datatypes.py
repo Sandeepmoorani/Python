96a6c6b2215dde1021a100e536be66f3b33d7b9c
Sandeep Moorani <sandeepmoorani222@gmail.com>
2023-05-01 08:37:40 +0500

    commited

---
+++ datatypes.py
@@ -0,0 +1,69 @@
+print("Hello world")
+x=2
+y="Sandeep"
+print(type(x))
+print(type(y))
+
+
+x=" Sandeep Moorani"
+print("hello!"+x)
+
+
+
+x=5 
+y=4
+z=x+y
+print(x+y)
+print(10+20)
+
+
+# strings
+x='Hello'
+y="Hello"
+
+x="""hello iam a software emgineer and iam learning first time programming language """
+print(x)
+
+
+# Numbers
+
+x=1
+y=1.1
+z=-3
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Tuples   
+#  lists 
+# lists are almost same but in lists we write variable value in Square brackets [] but in tuples round brackets ()
+
+# tuples are used to store multiple items in a single variable 
+
+
+
+mytup=("HI","Its","Me")
+print(mytup)
+
+
+lists=["abc","xyz","srt"]
+print(lists)
+
+# Dictionary
+ 
+# Dictionaries are used to store data values in key:value pairs. 
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+dict={"Lachman":"Singh","Sandeep":"Moorani","Naresh":"Netani"}
+print(dict)
+
+
+# sets
+
+# A set is a collection which is unordered, unchangeable*, and unindexed. 
+# * Note: Set items are unchangeable, but you can remove items and add new items. 
+# Sets are written with curly brackets. 
+
+sets={1,2,34,5,2}
+print(sets)
