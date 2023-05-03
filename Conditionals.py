4ee7ef69b8b12b6e9a211cb2749807415c99eb1f
Sandeep Moorani <sandeepmoorani222@gmail.com>
2023-05-03 08:09:16 +0500

    commit made

---
+++ Conditionals.py
@@ -0,0 +1,16 @@
+# if statement
+
+# x=10
+# y=20
+# if x<y:
+#     print("x is less than y" )
+a = 33
+b = 200
+if b > a: 
+  print("b is greater than a")
+
+elif a==b:
+      print("a equals to b")
+
+else:
+    print("a is greater than b")
