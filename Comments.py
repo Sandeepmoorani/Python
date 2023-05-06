2ef3798c0e14faa400708b696722651e90a87cba
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-06 18:03:54 +0500

    Committed

---
+++ Comments.py
@@ -0,0 +1,30 @@
+# A comment is a part of the coding file that the programmer does not want to execute,
+#  rather the programmer uses it to either explain a block of code or to avoid the execution
+#  of a specific part of code while testing.
+
+
+#This is a 'Single-Line Comment'
+print("This is a print statement.")
+
+print("Hello World !!!") #Printing Hello World
+
+#It will execute a block of code if a specified condition is true.
+#If the codition is false than it will execute another block of code.
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
+
+
+
+    # The use of multiline string.
+
+print("""This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code.""") 
+
+
+"""This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code."""
