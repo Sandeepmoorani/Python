4c5bf0bf3068f7c01bcd1f8fe08cc64a41a65b51
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-06 18:49:47 +0500

    commit added

--- Dataconv.py
+++ Dataconv.py
@@ -28,3 +28,24 @@ num3 = complex(num1)
 
 print(num2)
 print(num3)
+
+# ****************************************
+
+
+
+# Type Casting
+# Similar to type conversion, type casting is done when we want to specify a type on a variable. 
+
+# Example:
+
+str1 = "7"          
+str2 = "3.142"
+str3 = "13"
+num1 = 29
+num2 = 6.67
+
+print(int(str1))
+print(float(str2))
+print(float(str3))
+print(str(num1))
+print(str(num2))
