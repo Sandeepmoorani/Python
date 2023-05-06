19b6243c278c53710bb798bd9a907992b548b35d
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-06 18:46:34 +0500

    committed

---
+++ Dataconv.py
@@ -0,0 +1,30 @@
+# Data Conversion
+# The process of converting numeric data from one type to another is called as type conversion.
+# To convert from integer to float, we use float() function.
+ 
+
+# To convert from integer to complex, we use the complex() function.
+
+num1 = -25
+num2 = float(num1)
+num3 = complex(num1)
+
+print(num2)
+print(num3)
+
+# ********************8
+
+a=20
+b=str(a)
+c=complex(a)
+print(b)
+print(c)
+
+# *************************
+
+num1 = 8.4
+num2 = int(num1)
+num3 = complex(num1)
+
+print(num2)
+print(num3)
