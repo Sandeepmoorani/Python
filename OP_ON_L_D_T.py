7c5f73439030f887ee0eaede78ea8d9364d28952
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-06 18:04:37 +0500

    commit added

---
+++ OP_ON_L_D_T.py
@@ -0,0 +1,55 @@
+# Operations, Methods, and Functions
+# String Methods, Operations, and Functions
+
+# .upper()
+
+# .lower()
+
+# .split()
+
+# .format()
+
+# .isnumeric()
+
+# .isalpha()
+
+# .replace()
+
+# string index [ ]
+
+# len()
+
+# List Operations and Methods
+
+# .reverse()
+
+# .extend()
+
+# .insert()
+
+# .append()
+
+# .remove()
+
+# .sort()
+
+# list comprehensions [ ]
+
+# list comprehensions [ ] with if condition
+
+# Dictionary Operations and Methods
+
+# .items()
+
+# .update()
+
+# .keys()
+
+# .values()
+
+# .copy()
+
+# dictionary[key]
+
+# dictionary[key] = value 
+
