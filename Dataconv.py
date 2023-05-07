15d1974681694d3c5e1ed1333908e400c62dbfed
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-07 09:08:23 +0500

    committed

--- Dataconv.py
+++ Dataconv.py
@@ -1,3 +1,4 @@
+
 # Data Conversion
 # The process of converting numeric data from one type to another is called as type conversion.
 # To convert from integer to float, we use float() function.
@@ -48,4 +49,56 @@ print(int(str1))
 print(float(str2))
 print(float(str3))
 print(str(num1))
+
+# Data Conversion
+# The process of converting numeric data from one type to another is called as type conversion.
+# To convert from integer to float, we use float() function.
+ 
+
+# To convert from integer to complex, we use the complex() function.
+
+num1 = -25
+num2 = float(num1)
+num3 = complex(num1)
+
+print(num2)
+print(num3)
+
+# ********************8
+
+a=20
+b=str(a)
+c=complex(a)
+print(b)
+print(c)
+
+# *************************
+
+num1 = 8.4
+num2 = int(num1)
+num3 = complex(num1)
+
+print(num2)
+print(num3)
+
+# ****************************************
+
+
+
+# Type Casting
+# Similar to type conversion, type casting is done when we want to specify a type on a variable. 
+
+# Example:
+
+str1 = "7"          
+str2 = "3.142"
+str3 = "13"
+num1 = 29
+num2 = 6.67
+
+print(int(str1))
+print(float(str2))
+print(float(str3))
+print(str(num1))
+
 print(str(num2))
