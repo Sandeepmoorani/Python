4dc333c7fefb133b76eb81e9b173b7d7174535c7
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-09 01:26:41 +0500

    second commit

--- Conditionals.py
+++ Conditionals.py
@@ -4,13 +4,34 @@
 # y=20
 # if x<y:
 #     print("x is less than y" )
-a = 33
-b = 200
-if b > a: 
-  print("b is greater than a")
+# a = 33
+# b = 200
+# if b > a: 
+#   print("b is greater than a")
 
-elif a==b:
-      print("a equals to b")
+# elif a==b:
+#       print("a equals to b")
 
-else:
-    print("a is greater than b")
+# else:
+#     print("a is greater than b")
+
+
+# age=input("enter your age:")
+# age=int(age)
+# if age>=18:
+#   print("your are eligible")
+# else:
+#     print("your are not eligible")
+
+# *******************************
+
+# WinNum=5
+# GuessNum=int(input("Guess number :"))
+# if GuessNum==WinNum:
+#   print("your Win !!!")
+# elif GuessNum<WinNum:
+#   print("To Low")
+# elif GuessNum>WinNum:
+#   print("To high")
+# else:
+#   print("Dont worry next time")
