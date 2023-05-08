ed06bf1130641359e6cfae6b3725955af86798ff
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-09 01:44:55 +0500

    third commit

--- Conditionals.py
+++ Conditionals.py
@@ -35,3 +35,37 @@
 #   print("To high")
 # else:
 #   print("Dont worry next time")
+
+# *******************************
+
+# and operator  when both conditions are ture 
+
+# Name = 'Sandeep'
+# RollNo=15
+# if Name=='Sandeep' and RollNo==15:
+#   print("Condition ture!")
+# else:
+#   print("Condition false!")
+
+
+# or operator  when one  conditions is ture 
+
+#   Name = 'Sandeep'
+# RollNo=15
+# if Name=='Sandeep' or RollNo==21:
+#   print("Condition ture!")
+# else:
+#   print("Condition false!")
+
+# ********************************
+
+
+# Name,age=input("enter your name and age:").split()
+User_Name=input("enter your name:")
+User_age=int(input("enter your age:"))
+if User_age >=10 and (User_Name[0]=='a' or User_Name[0]=='A'):
+  print("You can watch movie ")
+else:
+  print("you cannot watch movie")
+
+
