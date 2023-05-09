23591e1f07b27c53e8fc8542503df45367594944
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-09 22:18:11 +0500

    sec commit

--- Loops.py
+++ Loops.py
@@ -1,6 +1,55 @@
 # while loop 
 
-i = 1
-while i < 6: 
- print(i) 
- i += 1 
+# i = 1
+# while i < 6: 
+#  print(i) 
+#  i += 1 
+
+
+# a=1
+# while a<=10:
+#     print(f"Sandeep Moorani {a}")
+#     a=a+1
+
+# sum of 10 numbers
+
+# total=0
+# i=1
+# while i<=20:
+#     # total += i
+#     total=total+i
+#     i= i + 1
+#     print(total)
+
+
+# total=0
+# user_num=int(input("enter your number :"))
+# i=1
+
+# while i<=user_num:
+#     total+=i
+    
+#     i+=1
+#     print(total)
+
+# ************************
+
+# n=input("enter your digits more than 1:")
+# # int(n[i]) 
+# i=0
+# total=0
+# while i<len(n):
+#     total+=int(n[i])
+#     i+=1
+#     print(total)
+    
+
+    # ****************************
+name=input("Enter your name please:")
+i=0
+temp_var=""
+while i<len(name):
+    if name[i] not in temp_var:
+        temp_var+=name[i]
+        print(f"{name[i]} : {name.count(name[i])}")
+    i+=1
