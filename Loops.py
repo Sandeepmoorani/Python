c573178c03385b78ce952936556ac4e083ad27b0
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-12 21:23:21 +0500

    seventh

--- Loops.py
+++ Loops.py
@@ -68,6 +68,19 @@
 # while True:
 #     print("Good Night")
 
+# ***********************
+
+# n=input("enter your digits more than 1:")
+# # int(n[i]) 
+# i=0
+# total=0
+# for i in range(len(n)):
+#     total+=int(n[i])
+#     i+=1
+#     print(total)
+
+
+
 
 
 
@@ -98,9 +111,17 @@
 
 
 
-n=int(input("Enter your number:"))
-sum=0
+# n=int(input("Enter your number:"))
+# sum=0
 
-for i in range(1,n+1):
-    sum+=i
-    print(sum)
+# for i in range(1,n+1):
+#     sum+=i
+#     print(sum)
+
+
+n=input("enter your number :")
+
+total=0
+for i in range(0,len(n)):
+    total+= int(n[i])
+    print(total)
