b872528023291f9b6aff2c9899db9a5b37eb8a50
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-13 13:16:30 +0500

    first commit

---
+++ Functions.py
@@ -0,0 +1,21 @@
+# Functions 
+
+# name = "Sandeep"
+# print(len(name))
+
+
+def sum(a,b):
+    return a+b
+
+# total=sum(1,2)
+# print(total)
+
+# x= int(input("Enter first number :"))
+# y= int(input("Enter second number :"))
+
+# total_sum= sum(x,y)
+# print(total_sum)
+ 
+first_name= input("Enter first name : ")
+last_name= input("Enter last name  : ")
+print(sum(first_name,last_name))
