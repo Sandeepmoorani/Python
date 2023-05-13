c2d2d79cee335509fa0f40c259064c3bf0afb113
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-13 13:23:57 +0500

    second commit

--- Functions.py
+++ Functions.py
@@ -3,9 +3,11 @@
 # name = "Sandeep"
 # print(len(name))
 
+# 888888888888888888888
 
-def sum(a,b):
-    return a+b
+
+# def sum(a,b):
+#     return a+b
 
 # total=sum(1,2)
 # print(total)
@@ -16,6 +18,23 @@ def sum(a,b):
 # total_sum= sum(x,y)
 # print(total_sum)
  
-first_name= input("Enter first name : ")
-last_name= input("Enter last name  : ")
-print(sum(first_name,last_name))
+# first_name= input("Enter first name : ")
+# last_name= input("Enter last name  : ")
+# print(sum(first_name,last_name))
+
+
+# ******************************
+
+# def last_char(name):
+#     return name[-1]
+# print(last_char("Sandeep"))
+
+
+# *******************************
+
+def even_odd(num):
+    if num%2 ==0:
+        return "even"
+    else:
+        return "odd"
+print(even_odd(1))
