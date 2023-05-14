8efee77438e2fd258dd2940299abb7c4f774af0a
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-14 13:57:19 +0500

    first commit

---
+++ List.py
@@ -0,0 +1,33 @@
+# List Declarat
+ 
+list1 = [1, 2, "Python", "Program", 15.9]      
+list2 = ["Sandeep", "Saeed", "Qurby", "Ali"]   
+  
+
+print(list1)  
+print(list2)  
+  
+
+
+# -> Characteristics of Lists
+
+# The list has the following characteristics:
+# The lists are ordered.
+# The element of the list can access by index.
+# The lists are the mutable type.
+# The lists are mutable types.
+# A list can store the number of various elements.
+
+
+# list_varible(start:stop:step)    
+
+
+# The {start} denotes the starting [index] position of the list.
+# The {stop} denotes the last index position of the list.
+# The {step} is used to skip the nth element within a start:stop
+
+# Repitation of lists 
+list1 = [12, 14, 16, 18, 20]  
+# repetition operator *  
+l = list1 * 2  
+print(l)
