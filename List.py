22183143bf904681b63653e9fbde8d649f296510
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-16 18:18:26 +0500

    commit added

--- List.py
+++ List.py
@@ -28,14 +28,26 @@
 # Adding elements to the list 
 
 #Declaring the empty list    
-l =[]    
-#Number of elements will be entered by the user      
-n = int(input("Enter the number of elements in the list:"))    
-# for loop to take the input    
-for i in range(0,n):       
-    # The input is taken from the user and added to the list as the item    
-    l.append(input("Enter the item:"))       
-print("printing the list items..")     
-# traversal loop to print the list items      
-for i in l:     
-    print(i, end = "  ")   
+# l =[]    
+# #Number of elements will be entered by the user      
+# n = int(input("Enter the number of elements in the list:"))    
+# # for loop to take the input    
+# for i in range(0,n):       
+#     # The input is taken from the user and added to the list as the item    
+#     l.append(input("Enter the item:"))       
+# print("printing the list items..")     
+# # traversal loop to print the list items      
+# for i in l:     
+#     print(i, end = "  ")   
+
+
+# removing elements from list 
+
+list = [0,1,2,3,4]       
+print("printing original list: ");      
+for i in list:      
+    print(i,end=" ")      
+list.remove(2)      
+print("\nprinting the list after the removal of first element...")      
+for i in list:      
+    print(i,end=" ")    
