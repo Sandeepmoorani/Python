a6a365212931a958d17eb6943bb3fc704fc2ed26
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-18 01:02:48 +0500

    committed

--- List.py
+++ List.py
@@ -67,33 +67,51 @@
 
 
 
-list1 = [1,2,2,3,55,98,65,65,13,29]    
-# Declare an empty list that will store unique values    
-list2 = []    
-for i in list1:    
-    if i not in list2:    
-        list2.append(i)    
-print(list2)    
+# list1 = [1,2,2,3,55,98,65,65,13,29]    
+# # Declare an empty list that will store unique values    
+# list2 = []    
+# for i in list1:    
+#     if i not in list2:    
+#         list2.append(i)    
+# print(list2)    
 
 
 
-# Write a program to find the sum of the element in the list.
+# # Write a program to find the sum of the element in the list.
 
 
 
-list1 = [3,4,5,9,10,12,24]    
-sum = 0    
-for i in list1:    
-    sum = sum+i        
-print("The sum is:",sum)  
+# list1 = [3,4,5,9,10,12,24]    
+# sum = 0    
+# for i in list1:    
+#     sum = sum+i        
+# print("The sum is:",sum)  
 
 
-# Write the program to find the lists consist of at least one common element.
+# # Write the program to find the lists consist of at least one common element.
 
 
-list1 = [1,2,3,4,5,6]    
-list2 = [7,8,9,2,10]    
-for x in list1:    
-    for y in list2:    
-        if x == y:    
-            print("The common element is:",x)    
+# list1 = [1,2,3,4,5,6]    
+# list2 = [7,8,9,2,10]    
+# for x in list1:    
+#     for y in list2:    
+#         if x == y:    
+#             print("The common element is:",x)    
+
+
+from turtle import *
+
+color('brown', 'red')
+begin_fill()
+left(140)
+forward(111.65)
+for i in range(200):
+    right(1)
+    forward(1)
+left(120)
+for i in range(200):
+    right(1)
+    forward(1)
+forward(111.65)
+end_fill()
+done()
