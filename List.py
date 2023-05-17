16232cde33aa632de0f64d423fc6cdc9a64d44cc
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-17 12:57:31 +0500

    commit added

--- List.py
+++ List.py
@@ -86,3 +86,14 @@ sum = 0
 for i in list1:    
     sum = sum+i        
 print("The sum is:",sum)  
+
+
+# Write the program to find the lists consist of at least one common element.
+
+
+list1 = [1,2,3,4,5,6]    
+list2 = [7,8,9,2,10]    
+for x in list1:    
+    for y in list2:    
+        if x == y:    
+            print("The common element is:",x)    
