19d4afec5831bf31a6f370a09959eb74710b5bde
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-19 12:37:35 +0500

    committed

--- dictionaries.py
+++ dictionaries.py
@@ -107,14 +107,41 @@ print("Salary : %d" %Employee["salary"])
 print("Company : %s" %Employee["Company"])    
 
 
-# Employee = {"Name": "David", "Age": 30, "salary":55000,"Company":"GOOGLE"}      
-# print(type(Employee))      
-# print("printing Employee data .... ")      
-# print(Employee)      
-print("Enter the details of the new employee....");      
-Employee["Name"] = input("Name: ");      
-Employee["Age"] = int(input("Age: "));      
-Employee["salary"] = int(input("Salary: "));      
-Employee["Company"] = input("Company:");      
-print("printing the new data");      
-print(Employee)   
+# ******************  input from user ***********************************  
+
+# print("Enter the details of the new employee....");      
+# Employee["Name"] = input("Name: ");      
+# Employee["Age"] = int(input("Age: "));      
+# Employee["salary"] = int(input("Salary: "));      
+# Employee["Company"] = input("Company:");      
+# print("printing the new data");      
+# print(Employee) 
+
+
+
+# ********************************* Built-in Dictionary methods *****************
+
+# clear() method  
+
+dict = {1: "Microsoft", 2: "Google", 3: "Facebook", 4: "Amazon", 5: "Flipkart"}  
+print(dict)
+dict.clear()  
+print(dict)  
+
+
+# copy() method  
+
+
+dict = {1: "Microsoft", 2: "Google", 3: "Facebook", 4: "Amazon", 5: "Flipkart"}  
+dict_demo = dict.copy()  
+print(dict_demo)  
+
+
+
+# pop() method  
+
+
+dict = {1: "Microsoft", 2: "Google", 3: "Facebook", 4: "Amazon", 5: "Flipkart"}  
+dict_demo = dict.copy()  
+x = dict_demo.pop(1)  
+print(x)  
