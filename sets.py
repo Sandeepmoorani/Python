e153daa26106dc638bdc1cd6bd5247695ff939f3
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-19 18:38:51 +0500

    first commit

---
+++ sets.py
@@ -0,0 +1,31 @@
+# set data type 
+# unordered collection of items 
+
+# no indexing 
+# no repeating element in sets only one times count 
+
+x={1,2,3,4,5}
+print(x)
+
+
+# remove duplicate 
+l=[1,2,3,3,4,4,32,23,3,2,2,3,45,5,6,7,8,8,9,8]
+x2=list(set(l))
+print(x2)
+
+
+# add element in sets 
+
+x.add(6)
+x.add(7)
+x.add(8)
+x.add(9)
+x.add(10)
+x.add(11)
+print(x)
+
+
+# remove element in sets
+
+x.remove(11)
+print(x)
