63134d5f7f2fea14109e0abc3127b72d320e2c62
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-20 09:43:32 +0500

    commit added

---
+++ *args.py
@@ -0,0 +1,18 @@
+def total(*args):
+    total=0
+    for i in args:
+        total +=i
+    return total
+print(total(2,4,6))
+
+
+
+
+# **********************8
+
+def multipy(*args):
+    multipy=1
+    for i in args:
+        multipy *=i
+    return multipy
+print(multipy(2,3,4))
