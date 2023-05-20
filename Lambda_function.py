156c39b4136bcb14c1d8c6b54b90c9f58743e6b8
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-20 10:14:21 +0500

    commit added

--- Lambda_function.py
+++ Lambda_function.py
@@ -47,3 +47,38 @@ numbers_list = [2, 4, 5, 1, 3, 7, 8, 9, 10]
 squared_list = list(map( lambda num: num ** 2 , numbers_list ))  
   
 print( squared_list )  
+
+
+# **************************** lambda with list comprehensation *****************
+
+#Code to calculate square of each number of list using list comprehension  
+squares = [lambda num = num: num ** 2 for num in range(0, 11)]  
+   
+for square in squares:  
+    print( square(), end = " ")  
+
+
+    # ************************ lambda with if elif **********************
+
+    # Code to use lambda function with if-else  
+
+Minimum = lambda x, y : x if (x < y) else y  
+   
+print(Minimum( 35, 74 )) 
+
+
+
+
+
+# Code to print the third-largest number of the given list using the lambda function  
+  
+my_List = [ [3, 5, 8, 6], [23, 54, 12, 87], [1, 2, 4, 12, 5] ]  
+  
+# sorting every sublist of the above list  
+sort_List = lambda num : ( sorted(n) for n in num )  
+  
+# Getting the third largest number of the sublist  
+third_Largest = lambda num, func : [ l[ len(l) - 2] for l in func(num)]  
+result = third_Largest( my_List, sort_List)  
+  
+print( result )  
