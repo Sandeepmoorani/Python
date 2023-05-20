244ace456bf1c41f2785848cef2821a1e1c86dbb
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-20 10:03:10 +0500

    commit added

--- *args.py
+++ *args.py
@@ -16,3 +16,13 @@ def multipy(*args):
         multipy *=i
     return multipy
 print(multipy(2,3,4))
+
+
+# *********************************** Lambda ***************************
+
+add= lambda a,b : a+b
+print(add(2,3))
+
+
+multipy = lambda x,y : x*y
+print(multipy(5,5))
