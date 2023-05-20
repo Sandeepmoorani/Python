7ef90e690d0fc0072b18e7219d7b4cfdbbd36c7c
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-20 23:09:03 +0500

    first commit

---
+++ Class.py
@@ -0,0 +1,22 @@
+class Student:
+    def __init__(self,name,rollno,district):
+        self.name=name
+        self.rollno=rollno
+        self.district=district
+
+s1= Student('Sandeep','21sw015','Tharparkar')
+print(s1.district)
+
+
+
+class Person:
+    def __init__(self,name,last_name,age):
+        self.name=name
+        self.last_name=last_name
+        self.age=age
+
+    def full_name(self):
+        return f"{self.name}  {self.last_name}"
+p1=Person('Sandeep','Moorani',20)
+print(p1.full_name())
+        
