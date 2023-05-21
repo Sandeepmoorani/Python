380fc4e494fb683bce12de5e862f1675593b5986
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-21 10:08:10 +0500

    third commit

--- Class.py
+++ Class.py
@@ -31,4 +31,37 @@ class Employee:
 # Creating a emp instance of Employee class  
 emp = Employee()    
 emp.display()    
+
+
+
+
+
+class Employee:  
+    def __init__(self, name, id):  
+        self.id = id  
+        self.name = name  
+  
+    def display(self):  
+        print("ID: %d \nName: %s" % (self.id, self.name))  
+  
+  
+emp1 = Employee("John", 101)  
+emp2 = Employee("David", 102)  
+  
+# accessing display() method to print employee 1 information  
+  
+emp1.display()  
+  
+# accessing display() method to print employee 2 information  
+emp2.display()  
+
+
+class Student:    
+    count = 0    
+    def __init__(self):    
+        Student.count = Student.count + 1    
+s1=Student()    
+s2=Student()    
+s3=Student()    
+print("The number of students:",Student.count)    
         
