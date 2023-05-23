6e3b82e19cbc3e29ca72c142c3286981a80944ab
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-23 17:48:29 +0500

    fourth commit

--- Inheretence.py
+++ Inheretence.py
@@ -58,3 +58,24 @@ d = Derived()
 print(d.Summation(10,20))  
 print(d.Multiplication(10,20))  
 print(d.Divide(10,20))  
+
+
+
+
+
+# The issubclass(sub,sup) method
+
+# The issubclass(sub, sup) method is used to check the relationships between the specified classes. It returns true if the first class is the subclass of the second class, and false otherwise.
+
+class Calculation1:  
+    def Summation(self,a,b):  
+        return a+b;  
+class Calculation2:  
+    def Multiplication(self,a,b):  
+        return a*b;  
+class Derived(Calculation1,Calculation2):  
+    def Divide(self,a,b):  
+        return a/b;  
+d = Derived()  
+print(issubclass(Derived,Calculation2))  
+print(issubclass(Calculation1,Calculation2))  
