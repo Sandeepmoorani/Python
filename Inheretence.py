1885a6d2451fc6198fdc26deb0c4a3c8b88e8d40
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-23 17:49:55 +0500

    commit made

--- Inheretence.py
+++ Inheretence.py
@@ -79,3 +79,23 @@ class Derived(Calculation1,Calculation2):
 d = Derived()  
 print(issubclass(Derived,Calculation2))  
 print(issubclass(Calculation1,Calculation2))  
+
+
+
+
+# The isinstance (obj, class) method
+
+
+# The isinstance() method is used to check the relationship between the objects and classes. It returns true if the first parameter, i.e., obj is the instance of the second parameter, i.e., class.
+
+class Calculation1:  
+    def Summation(self,a,b):  
+        return a+b;  
+class Calculation2:  
+    def Multiplication(self,a,b):  
+        return a*b;  
+class Derived(Calculation1,Calculation2):  
+    def Divide(self,a,b):  
+        return a/b;  
+d = Derived()  
+print(isinstance(d,Derived))  
