c46ccc3b162a5522661578c6bdc419cde11eac41
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-24 20:45:48 +0500

    commit added

--- Inheretence.py
+++ Inheretence.py
@@ -119,4 +119,25 @@ class Dog(Animal):
     def speak(self):  
         print("Barking")  
 d = Dog()  
-print(d.speak)
+d.speak
+
+
+# Real Life Example of method overriding
+
+
+class Bank:  
+    def getroi(self):  
+        return 10;  
+class SBI(Bank):  
+    def getroi(self):  
+        return 7;  
+  
+class ICICI(Bank):  
+    def getroi(self):  
+        return 8;  
+b1 = Bank()  
+b2 = SBI()  
+b3 = ICICI()  
+print("Bank Rate of interest:",b1.getroi());  
+print("SBI Rate of interest:",b2.getroi());  
+print("ICICI Rate of interest:",b3.getroi());  
