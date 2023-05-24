c8b623b92c57857b9ccd6b8e8c3690f780fdbdd9
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-24 20:47:26 +0500

    commit added

--- Inheretence.py
+++ Inheretence.py
@@ -141,3 +141,26 @@ b3 = ICICI()
 print("Bank Rate of interest:",b1.getroi());  
 print("SBI Rate of interest:",b2.getroi());  
 print("ICICI Rate of interest:",b3.getroi());  
+
+
+
+# ***************************************************************
+
+# Data abstraction in python
+
+
+# Abstraction is an important aspect of object-oriented programming. In python, we can also perform data hiding by adding the double underscore (___) as a prefix to the attribute which is to be hidden. After this, the attribute will not be visible outside of the class through the object.
+
+
+class Employee:  
+    __count = 0;  
+    def __init__(self):  
+        Employee.__count = Employee.__count+1  
+    def display(self):  
+        print("The number of employees",Employee.__count)  
+emp = Employee()  
+emp2 = Employee()  
+try:  
+    print(emp.__count)  
+finally:  
+    emp.display()  
