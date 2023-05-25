fd0b230b5dfebd50a66fd74c39c0bbfef4e06011
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-25 16:51:24 +0500

    first commit

---
+++ Built_in_fun.py
@@ -0,0 +1,17 @@
+# Python Built-in Functions
+
+# The Python built-in functions are defined as the functions whose functionality is pre-defined in Python.
+
+# Python abs() Function
+
+# The python abs() function is used to return the absolute value of a number. It takes only one argument, a number whose absolute value is to be returned. The argument can be an integer and floating-point number. If the argument is a complex number, then, abs() returns its magnitude.
+
+
+
+#  integer number     
+integer = -20  
+print('Absolute value of -40 is:', abs(integer))  
+  
+#  floating number  
+floating = -20.83  
+print('Absolute value of -40.83 is:', abs(floating))  
