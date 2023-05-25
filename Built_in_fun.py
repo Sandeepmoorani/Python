c28cc806aec3e7522bcfa35cbba18d8745937a8b
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-25 16:54:13 +0500

    third commit

--- Built_in_fun.py
+++ Built_in_fun.py
@@ -15,3 +15,43 @@ print('Absolute value of -40 is:', abs(integer))
 #  floating number  
 floating = -20.83  
 print('Absolute value of -40.83 is:', abs(floating))  
+
+
+# **************************************** all() ****************************
+
+
+# The python all() function accepts an iterable object (such as list, dictionary, etc.). It returns true if all items in passed iterable are true. Otherwise, it returns False. If the iterable object is empty, the all() function returns True.
+
+
+# all values true  
+k = [1, 3, 4, 6]  
+print(all(k))  
+  
+# all values false  
+k = [0, False]  
+print(all(k))  
+  
+# one false value  
+k = [1, 3, 7, 0]  
+print(all(k))  
+  
+# one true value  
+k = [0, False, 5]  
+print(all(k))  
+  
+# empty iterable  
+k = []  
+print(all(k))  
+
+
+
+# *********************************** bin() ***************************
+
+
+# The python bin() function is used to return the binary representation of a specified integer. A result always starts with the prefix 0b.
+
+
+
+x =  10  
+y =  bin(x)  
+print (y)  
