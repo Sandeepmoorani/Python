2b0ed1eb8fa7b0808040aa2943e0f8c479b61407
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-05-30 19:32:32 +0500

    commit made

--- Built_in_fun.py
+++ Built_in_fun.py
@@ -405,3 +405,18 @@ print(result)
 print(result2)  
 # Verify, is it string type?  
 print("is it string type:", type(result) is str)  
+
+
+# ********************************complex()********************************
+
+# Python complex() function is used to convert numbers or string into a complex number. This method takes two optional parameters and returns a complex number. The first parameter is called a real and second as imaginary parts.
+
+
+
+# Python complex() function example  
+# Calling function  
+a = complex(1) # Passing single parameter  
+b = complex(1,2) # Passing both parameters  
+# Displaying result  
+print(a)  
+print(b)  
