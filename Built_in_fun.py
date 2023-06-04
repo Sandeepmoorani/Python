750c85308b107a61e7de2ef051c9f76b90cf147b
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-04 10:03:17 +0500

    commit made

--- Built_in_fun.py
+++ Built_in_fun.py
@@ -773,3 +773,19 @@ print(student.name)
 val = id(student) # student class object  
 # Displaying result  
 print("Object id:",val)  
+
+
+
+# Python id() function example  
+l1 = [1,2,3,4]  
+l2 = [1,2,3,4]  
+l3 = [3,5,6,7]  
+# Calling function  
+id1 = id(l1)  
+id2 = id(l2)  
+id3 = id(l3)  
+# Displaying result  
+print((l1==l2),(l1==l3))  
+# Objects with the same values can have different ids  
+print((id1==id2),(id1==id3))  
+# l1 and l2 returns True, while id1 and id2 returns False  
