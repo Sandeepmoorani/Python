3be381b6d372fa8a0a1e7f52deaa265e85121c57
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-07 00:53:44 +0500

    second commit

--- file_io.py
+++ file_io.py
@@ -6,8 +6,10 @@ print(f.read())
 print(f'curser position -- {f.tell()}')
 
 print('before seek method :')
-f.seek()
+f.seek(2)
 print('after seek method')
 
 
 print(f'curser position -- {f.tell()}')
+print(f.read())
+f.close()
