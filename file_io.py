e3878069fcb4434092d80eab138da2f60f6b364c
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-07 01:01:32 +0500

    third commit

--- file_io.py
+++ file_io.py
@@ -1,3 +1,16 @@
+# open()        used to open the file
+# close()       close the file
+# read()        to read the file
+# readline()    only one line read
+# readlines()   read multiple lines
+# seek()        used to give postion to the curser
+# tell()        it tells position of the curser
+
+
+
+
+
+
 f=open('file.txt')
 
 print(f'curser position -- {f.tell()}')
@@ -13,3 +26,6 @@ print('after seek method')
 print(f'curser position -- {f.tell()}')
 print(f.read())
 f.close()
+
+# f.readline()
+# f.readlines()
