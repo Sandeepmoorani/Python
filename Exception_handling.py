47aecb15e8b757351db5c7a03421c17a9823a8ac
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-06 23:24:14 +0500

    second commit

--- Exception_handling.py
+++ Exception_handling.py
@@ -1,8 +1,15 @@
-try:
-    age= int(input("enter your age :"))
-except:
-    print("invalid input")
+while True:
+    try:
+        age= int(input("enter your age :"))
+        break
+    except ValueError:
+        print("invalid input")
+    except:
+        print("unexpected error....")
+
+
 if age <18:
     print("not adult")
 else:
     print("adult")
+    
