99996afafec7f8c1d91336a25788f21a2ea84114
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-07 21:03:56 +0500

    commit made

--- Exception_handling.py
+++ Exception_handling.py
@@ -1,20 +1,32 @@
-while True:
-    try:
-        age= int(input("enter your age :"))
-        break
-    except ValueError:
-        print("invalid input")
-    except:
-        print("unexpected error....")
-    else:
-        print("adult")
-        break
-    finally:
-        print("finally block")
+# while True:
+#     try:
+#         age= int(input("enter your age :"))
+#         break
+#     except ValueError:
+#         print("invalid input")
+#     except:
+#         print("unexpected error....")
+#     else:
+#         print("adult")
+#         break
+#     finally:
+#         print("finally block")
 
 
 # if age <18:
 #     print("not adult")
 # else:
 #     print("adult")
-    
+
+
+
+def reciprocal( num1 ):  
+    try:  
+        reci = 1 / num1  
+    except ZeroDivisionError:  
+        print( "We cannot divide by zero" )  
+    else:  
+        print ( reci )  
+# Calling the function and passing values  
+reciprocal( 4 )  
+reciprocal( 0 )  
