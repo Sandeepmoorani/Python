204636ec2b89b5530c5e62ede19202046e3b53b6
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-08 01:20:21 +0500

    third commit

--- csv_file.py
+++ csv_file.py
@@ -13,11 +13,30 @@
 
 # 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888
 
-from csv import DictReader
+# from csv import DictReader
 
-with open('file.CSV','r') as f:
+# with open('file.CSV','r') as f:
 
-    csv_reader = DictReader(f)
+#     csv_reader = DictReader(f)
 
-    for row in csv_reader:
-        print(row)
+#     for row in csv_reader:
+#         print(row)
+
+
+
+# 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+from csv import writer
+
+with open('fille1.csv','w') as f:
+
+    csv_writer= writer(f)
+
+    csv_writer.writerow(['name','age'])
+
+    csv_writer.writerow(['sandeep','20'])
+
+    csv_writer.writerow(['sohail','19'])
+
+
+    csv_writer.writerows([['name','age'],['sandeep','20'],['sohail','20'],['khushal','19']])
