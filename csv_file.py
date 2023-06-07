27c22eaa126dba4fa60777ba403e92fe10005c50
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-08 01:11:51 +0500

    sec commit

--- csv_file.py
+++ csv_file.py
@@ -1,12 +1,23 @@
-from csv import reader
+# from csv import reader
 
-with open ('file.csv', 'r') as f:
+# with open ('file.csv', 'r') as f:
 
-    csv_reader= reader(f)
+#     csv_reader= reader(f)
 
-    next(csv_reader)
+#     # next(csv_reader)
+
+#     for row in csv_reader:
+
+#         print(row)
+
+
+# 8888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+from csv import DictReader
+
+with open('file.CSV','r') as f:
+
+    csv_reader = DictReader(f)
 
     for row in csv_reader:
-
         print(row)
-
