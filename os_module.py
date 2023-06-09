2386cd6eadc5312160828599e07ea438268a9798
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-09 22:20:56 +0500

    commit added

--- os_module.py
+++ os_module.py
@@ -1,10 +1,27 @@
-import os
+# import os
 
-print(os.getcwd())
+# print(os.getcwd())
 
 # os.mkdir('python practice')
 
 
 # print(os.path.exists('python practice'))
 
-print(os.listdir())
+# print(os.listdir())
+
+
+# Python OS module provides the facility to establish the interaction between the user and the operating system. It offers many useful OS functions that are used to perform OS-based tasks and get related information about operating system.
+
+
+
+# The OS comes under Python's standard utility modules. This module offers a portable way of using operating system dependent functionality.
+
+# The Python OS module lets us work with the files and directories.
+
+# To work with the OS module, we need to import the OS module.  
+# import os  
+
+
+
+import os   
+print(os.name)   
