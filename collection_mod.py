ff156437b1694549f3255ab006d53b92eb2b521f
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-16 23:06:51 +0500

    Create collection_mod.py

---
+++ collection_mod.py
@@ -0,0 +1,4 @@
+# Python Collection Module
+# The Python collection module is defined as a container that is used to store collections of data, for example - list, dict, set, and tuple, etc. It was introduced to improve the functionalities of the built-in collection containers.
+
+# Python collection module was first introduced in its 2.4 release.
