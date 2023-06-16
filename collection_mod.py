d4b4f3efe91dcaecc3ffac632d1f8b2b4ed16ca3
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-16 23:08:15 +0500

    Update collection_mod.py

--- collection_mod.py
+++ collection_mod.py
@@ -2,3 +2,15 @@
 # The Python collection module is defined as a container that is used to store collections of data, for example - list, dict, set, and tuple, etc. It was introduced to improve the functionalities of the built-in collection containers.
 
 # Python collection module was first introduced in its 2.4 release.
+
+# There are different types of collection modules which are as follows:
+
+# namedtuple()
+# The Python namedtuple() function returns a tuple-like object with names for each position in the tuple. It was used to eliminate the problem of remembering the index of each field of a tuple object in ordinary tuples.
+
+
+
+# Examples:
+
+pranshu = ('James', 24, 'M')    
+print(pranshu)    
