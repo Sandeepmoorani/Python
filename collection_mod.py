e9eeea84dfd5e9629abdd71bfea3ce18d630cf43
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-19 22:51:43 +0500

    Update collection_mod.py

--- collection_mod.py
+++ collection_mod.py
@@ -41,3 +41,16 @@ number['two'] = 2
 print(number['three'])    
 
 # **************************************************************************************
+# Counter()
+# The Python Counter is a subclass of dictionary object which helps to count hashable objects.
+
+# Example
+
+from collections import Counter      
+c = Counter()    
+list = [1,2,3,4,5,7,8,5,9,6,10]      
+Counter(list)    
+Counter({1:5,2:4})      
+list = [1,2,4,7,5,1,6,7,6,9,1]      
+c = Counter(list)      
+print(c[1])     
