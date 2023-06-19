d82405aaa31c28019b0f66db9d8fb6d7d7bb62c8
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-19 22:49:50 +0500

    Update collection_mod.py

--- collection_mod.py
+++ collection_mod.py
@@ -34,3 +34,8 @@ for k,v in d1.items():
 
 defaultdict()
 # The Python defaultdict() is defined as a dictionary-like object. It is a subclass of the built-in dict class. It provides all methods provided by dictionary but takes the first argument as a default data type.
+from collections import defaultdict      
+number = defaultdict(int)      
+number['one'] = 1      
+number['two'] = 2      
+print(number['three'])    
