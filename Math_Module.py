7c8395cca75b76bcf135c5f4afe5bf3493fff79d
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-19 22:59:02 +0500

    Create Math_Module.py

---
+++ Math_Module.py
@@ -0,0 +1,4 @@
+# What is Math Module in Python?
+# Python has a built-in math module. It is a standard module, so we don't need to install it separately. We only have to import it into the program we want to use. We can import the module, like any other module of Python, using import math to implement the functions to perform mathematical operations.
+
+# Since the source code of this module is in the C language, it provides access to the functionalities of the underlying C library. 
