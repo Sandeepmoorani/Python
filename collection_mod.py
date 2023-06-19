d0fe20c82a0b945f9282b7ff54b6163d8b421069
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-19 22:50:51 +0500

    Update collection_mod.py

--- collection_mod.py
+++ collection_mod.py
@@ -39,3 +39,5 @@ number = defaultdict(int)
 number['one'] = 1      
 number['two'] = 2      
 print(number['three'])    
+
+# **************************************************************************************
