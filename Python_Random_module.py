ade452e3022b2cc822d5d37cebe0436f56c2a3f6
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-20 14:21:09 +0500

    added

--- Python_Random_module.py
+++ Python_Random_module.py
@@ -1,1 +1,3 @@
 # The Python Random module is a built-in module for generating random integers in Python. These numbers occur randomly and does not follow any rules or instructuctions. We can therefore use this module to generate random numbers, display a random item for a list or string, and so on.
+The random() Function
+# The random.random() function gives a float number that ranges from 0.0 to 1.0. There are no parameters required for this function. This method returns the second random floating-point value within [0.0 and 1] is returned.
