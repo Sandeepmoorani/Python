a4952996207f26b526c720e69959c3c620f60b3b
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-06-20 14:24:51 +0500

    Update Python_Random_module.py

--- Python_Random_module.py
+++ Python_Random_module.py
@@ -21,3 +21,11 @@ print( num )
 # ***********************************************************************
 The randrange() Function
 # The random.randrange() function selects an item randomly from the given range defined by the start, the stop, and the step parameters. By default, the start is set to 0. Likewise, the step is set to 1 by default.
+
+
+# To generate value between a specific range  
+# import random    
+num = random.randrange(1, 10)    
+print( num )    
+num = random.randrange(1, 10, 2)    
+print( num )                
