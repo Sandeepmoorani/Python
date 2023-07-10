ac7e995c0e5d7f184fe39d9a30bfc6d62d3b9afe
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-07-10 21:23:24 +0500

    Update statistics+module.py

--- statistics+module.py
+++ statistics+module.py
@@ -1,2 +1,6 @@
 # Python statistics module
 # Python statistics module provides the functions to mathematical statistics of numeric data. There are some popular statistical functions defined in this module.
+
+
+# mean() function
+# The mean() function is used to calculate the arithmetic mean of the numbers in the list.
