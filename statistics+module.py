e9bbcac298c59202b3b00aed84987c703640c79c
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-07-10 21:22:39 +0500

    made

---
+++ statistics+module.py
@@ -0,0 +1,2 @@
+# Python statistics module
+# Python statistics module provides the functions to mathematical statistics of numeric data. There are some popular statistical functions defined in this module.
