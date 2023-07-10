109528ae9ec16e8f241f4eca6677e32e870551f9
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-07-10 21:27:08 +0500

    Update statistics+module.py

--- statistics+module.py
+++ statistics+module.py
@@ -39,4 +39,15 @@ print("Median of data-set is : % s "
 # The mode() function returns the most common data that occurs in the list.
 
 
+# Example
 
+import statistics     
+# declaring a simple data-set consisting of real valued positive integers.   
+dataset =[2, 4, 7, 7, 2, 2, 3, 6, 6, 8]     
+# Printing out the mode of given data-set   
+print("Calculated Mode % s" % (statistics.mode(dataset)))  
+
+
+# Output:
+
+# Calculated Mode 2
