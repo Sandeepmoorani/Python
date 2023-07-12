0b76c0493de73d2dec96d50ed0b9b5d39a35dbb0
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-07-12 21:37:55 +0500

    Create Python_sys_module.py

---
+++ Python_sys_module.py
@@ -0,0 +1,2 @@
+Python sys module
+The python sys module provides functions and variables which are used to manipulate different parts of the Python Runtime Environment. It lets us access system-specific parameters and functions.
