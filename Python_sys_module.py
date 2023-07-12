4eb79412e395cdd6546b2eed8fe9d7fa40656eff
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-07-12 21:39:18 +0500

    Update Python_sys_module.py

--- Python_sys_module.py
+++ Python_sys_module.py
@@ -7,3 +7,9 @@
 # First, we have to import the sys module in our program before running any functions.
 
 # sys.modules
+# his function provides the name of the existing python modules which have been imported.
+
+# sys.argv
+
+# This function returns a list of command line arguments passed to a Python script. The name of the script is always the item at index 0, and the rest of the arguments are stored at subsequent indices.
+
