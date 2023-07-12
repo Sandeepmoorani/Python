cf0561ed884f55b4b579c9e2be023082299eb2f9
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-07-12 21:38:46 +0500

    Update Python_sys_module.py

--- Python_sys_module.py
+++ Python_sys_module.py
@@ -1,2 +1,9 @@
-Python sys module
-The python sys module provides functions and variables which are used to manipulate different parts of the Python Runtime Environment. It lets us access system-specific parameters and functions.
+# Python sys module
+# The python sys module provides functions and variables which are used to manipulate different parts of the Python Runtime Environment. It lets us access system-specific parameters and functions.
+
+
+# import sys
+
+# First, we have to import the sys module in our program before running any functions.
+
+# sys.modules
