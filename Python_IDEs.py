61af8f6bed8e1b67c041dc74d824f77050a6a926
Sandeep Moorani <99572232+Sandeepmoorani@users.noreply.github.com>
2023-07-14 13:09:34 +0500

    Create Python_IDEs.py

---
+++ Python_IDEs.py
@@ -0,0 +1,4 @@
+Python IDEs
+The term "IDE" refers for "Integrated Development Environment," which is a coding tool that aids in automating the editing, compiling, testing, and other steps of an SDLC while making it simple for developers to execute, write, and debug code.
+
+It is specifically made for software development and includes a number of tools that are used in the creation and testing of the software.
